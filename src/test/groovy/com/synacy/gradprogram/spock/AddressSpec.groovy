d9bd6091c68b2492316bac942eebe41f8666c3df
Irvin Gil Mercado <irvin.mercado@synacy.com>
2022-07-21 16:38:04 +0800

    test(AddressSpec.groovy) added test methods for getters and setters of Country, City, and Street

--- src/test/groovy/com/synacy/gradprogram/spock/AddressSpec.groovy
+++ src/test/groovy/com/synacy/gradprogram/spock/AddressSpec.groovy
@@ -3,66 +3,81 @@ package com.synacy.gradprogram.spock
 import spock.lang.Specification
 
 class AddressSpec extends Specification {
-//    def "test getCountry"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test setCountry"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test getCity"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test setCity"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test getStreet"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test setStreet"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test toString"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
+    def "getCountry() should return the expected country address"() {
+        given:
+        Address address = new Address("Philippines", "Cebu", "pope john paul blah")
+
+        when:
+        String countryLocation = address.getCountry()
+
+        then:
+        countryLocation == "Philippines"
+
+    }
+
+    def "setCountry() should return the name of the country being set"() {
+
+        when:
+        Address address = new Address("Philippines", "Maasin", "Mari Clara")
+        address.setCountry("Japan")
+
+        then:
+        address.getCountry() == "Japan"
+
+    }
+
+    def "getCity() should return the expected city address"() {
+        given:
+        Address address = new Address("Philippines", "Maasin", "Mantahan")
+
+        when:
+        String cityLocation = address.getCity()
+
+
+        then:
+        cityLocation == "Maasin"
+    }
+
+    def "setCity() should return the name of the city being set "() {
+        when:
+        Address address = new Address("Philippines", "Maasin", "Mantahan")
+        address.setCity("Cebu City")
+
+        then:
+        address.getCity() == "Cebu City"
+    }
+
+    def "getStreet() should return the expected street name"() {
+        given:
+        Address address = new Address("Philippines", "Cebu City","Kasambagan")
+
+        when:
+        String streetName = address.getStreet()
+
+        then:
+        streetName ==  "Kasambagan"
+    }
+
+    def "setStreet() should return the name of the street being set"() {
+        when:
+        Address address = new Address("Philippines", "Cebu City", "Talamban")
+        address.setStreet("Guadalupe")
+
+        then:
+        address.getStreet() == "Guadalupe"
+
+    }
+
+    def "toString() should return a string with the Country, City, and Street in a single phrase"() {
+
+        given:
+        Address address = new Address("Philippines", "Maasin City", "Mantahan")
+
+        when:
+        String expectedToString = address.toString()
+
+        then:
+        expectedToString == "Country: Philippines, City: Maasin City, Street: Mantahan"
+
+    }
 }
