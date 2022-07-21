a2f6eac9362300629c4812c1021fb634f573e514
Irvin Gil Mercado <irvin.mercado@synacy.com>
2022-07-21 20:29:15 +0800

    test(PersonSpec.groovy) write test method for getters and setters Name and Age

--- src/test/groovy/com/synacy/gradprogram/spock/PersonSpec.groovy
+++ src/test/groovy/com/synacy/gradprogram/spock/PersonSpec.groovy
@@ -1,43 +1,59 @@
 package com.synacy.gradprogram.spock
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class PersonSpec extends Specification {
-//    def "test getName"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test setName"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test getAge"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test setAge"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
+    static int personAge = 0;
+    static Sex personSex = Sex.MALE;
+    Address address = new Address("CountryName", "CityName", "StreetName")
+    Person person = new Person("PersonName", personAge, address, personSex)
+
+    def "getName() should return string name being set"() {
+        when:
+        person.setName("Zuckerberg")
+
+        then:
+        person.getName() == "Zuckerberg"
+    }
+
+    def "setName() should return expected name"() {
+        when:
+        person.setName("Enrique")
+        String resultOfGetName = person.getName()
+
+        then:
+        resultOfGetName == "Enrique"
+    }
+
+    @Unroll
+    def "getAge() should return int age being set: #expectedAge"() {
+        when:
+        Person TestAge = new Person("PersonName", expectedAge, address, personSex)
+
+        then:
+        TestAge.getAge() == expectedAge
+
+
+        where:
+        expectedAge << (1..100)
+
+
+    }
+
+    @Unroll
+    def "setAge() should return expected int age: #expectedAge"() {
+
+        when:
+        Person TestAge = new Person("PersonName", 12, address, personSex)
+        TestAge.setAge(expectedAge)
+
+        then:
+        TestAge.getAge() == expectedAge
+
+        where:
+        expectedAge << (1..50)
+    }
 //
 //    def "test getAddress"() {
 //        given:
