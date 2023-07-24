b2126200be1d53deccf765b61a04c41b0a4a56f0
clark <clark.tabar@synacy.com>
2023-07-24 19:46:01 +0800

    Added the refundService.calculateRefund() implementation.

--- src/main/java/com/synacy/gradprogram/spock/exercise/RefundService.java
+++ src/main/java/com/synacy/gradprogram/spock/exercise/RefundService.java
@@ -2,19 +2,35 @@ package com.synacy.gradprogram.spock.exercise;
 
 import java.math.BigDecimal;
 
+
 public class RefundService {
 
-  public BigDecimal calculateRefund() {
-
+  public double calculateRefund(CancelOrderRequest cancelOrderRequest, Order order) {
     // TODO: Implement me. Full refund if cancel reason is due to damaged item.
     //  Also full refund if the order was cancelled within 3 days of order date, else refund half of the total cost.
+    OrderRepository orderRepository = new OrderRepository();
 
+    orderRepository.fetchOrderById(cancelOrderRequest.getOrderId());
 
-    return null;
+    RefundRequest refund = new RefundRequest();
+    RefundRepository refundRepository = new RefundRepository();
+
+    if(cancelOrderRequest.getReason() == CancelReason.DAMAGED){
+      refund.setRefundAmount(order.getTotalCost());
+
+      refundRepository.saveRefundRequest(refund);
+
+    }
+
+    return refund.getRefundAmount();
   }
 
-  private void createAndSaveRefundRequest() {
+  private void createAndSaveRefundRequest(CancelOrderRequest cancelOrderRequest) {
     // TODO: Implement me. Creates a TO_PROCESS refund request and saves it to the database
+
+
+
+
   }
 
 }
